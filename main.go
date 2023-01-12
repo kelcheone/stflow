cc73c85c3c9295db168a7fb3073c3e843291f75c
kelche <kelcheone@gmail.com>
2023-01-12 17:44:26 +0300

    feat: svete data

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// get time it takes to run the program
 	start := time.Now()
-	get_all_pages("pandas")
+	get_all_pages("svelte")
 	fmt.Printf("Time taken Golang: %v \n", time.Since(start))
 
 }
