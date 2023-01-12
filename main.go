d40bad1e317ed70e409908a66e19ac32d078a52a
kelche <kelcheone@gmail.com>
2023-01-12 14:21:10 +0300

    feat: scrape go  data

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// get time it takes to run the program
 	start := time.Now()
-	get_all_pages("android")
+	get_all_pages("go")
 	fmt.Printf("Time taken Golang: %v \n", time.Since(start))
 
 }
